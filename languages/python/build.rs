566f29d80ec37cd09ab79aa705775e54ad9c3770
Isabella Tromba <isabella.tromba@gmail.com>
2022-01-27 13:26:55 -0800

    first training from python wip

--- languages/python/build.rs
+++ languages/python/build.rs
@@ -1,5 +1,9 @@
 fn main() {
 	let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+	if target_os.as_str() == "macos" {
+		println!("cargo:rustc-cdylib-link-arg=-undefined");
+		println!("cargo:rustc-cdylib-link-arg=dynamic_lookup");
+	}
 	if target_os.as_str() == "windows" {
 		println!(
 			"cargo:rustc-link-search=native={}",
